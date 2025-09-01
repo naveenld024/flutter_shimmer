c1b3639c7952e6ca86938bfeb84a388e6a5643d0
naveen <naveenld024@gmail.com>
2025-09-01 12:05:34 +0400

    Update flutter_shimmer_test.dart to import AutoShimmer from the renamed flutter_auto_shimmer package.

--- test/flutter_shimmer_test.dart
+++ test/flutter_shimmer_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auto_shimmer/flutter_shimmer.dart' show AutoShimmer;
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_shimmer/flutter_shimmer.dart';
 
 void main() {
   test('AutoShimmer test', () {
